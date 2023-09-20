7a812bdf6caaef5ecc369a1cad535c38f30cb4e5
Diego <dmercadoff@gmail.com>
2023-09-20 12:27:03 -0400

    primer push

--- hms/doctor/include/config.php
+++ hms/doctor/include/config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'hms');
+define('DB_USER','u583014169_HospitalSIS');
+define('DB_PASS' ,'gtaV19921963');
+define('DB_NAME', 'u583014169_HospitalSIS');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
